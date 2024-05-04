6ed4e37fc70d7721aeecb5f588f4cfe0897cc2d0
Alex Wein <awein@hashicorp.com>
2024-05-04 19:51:34 +0000

    wakelock didn't work, undoing it

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Tile from './Tile';
 
 import newBoard from '../../helpers/newBoard';
 import freqs from '../../data/freqs.json';
 
 export default function Grid({ options }) {
-  const [wakeLockEnabled, setWakeLockEnabled] = useState(false);
 
     useEffect(() => {
         // WaKe Lock VIA chatgpt
@@ -16,31 +15,6 @@ export default function Grid({ options }) {
         if (gameGrid) {
           gameGrid.style.gridTemplateColumns = `repeat(${options.nCols}, 1fr)`; 
         }
-
-        let wakeLock = null;
-
-        async function requestWakeLock() {
-          try {
-            if ('wakeLock' in navigator) {
-              // Request a screen wake lock
-              wakeLock = await navigator.wakeLock.request('screen');
-              setWakeLockEnabled(true);
-            }
-          } catch (error) {
-            console.error('Failed to request wake lock:', error);
-          }
-        }
-        // Request wake lock when component mounts
-        requestWakeLock();
-
-        // Release wake lock when component unmounts
-        return () => {
-          if (wakeLock) {
-            wakeLock.release();
-            setWakeLockEnabled(false);
-          }
-    };
-
 
       }, [options]); // Run this effect only once after the component mounts
 
