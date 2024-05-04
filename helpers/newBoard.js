1e8619e3169613655847af6c4b74bfd2721e4cdd
Alex Wein <awein@hashicorp.com>
2024-05-04 16:12:48 +0000

    changing board gen this looks better

--- helpers/newBoard.js
+++ helpers/newBoard.js
@@ -4,16 +4,23 @@ import bagOfLetters from './bagOfLetters';
 
 export default function newBoard(freqs, selectFreqs, nRows, nCols, seed) {
     const random = seedrandom(seed);
-    const randomSort = (a,b) => random() - .5; // chatgpt for random sorting an array
+    // const randomSort = (a,b) => Math.random() - .5; // chatgpt for random sorting an array
 
-    const board = bagOfLetters(freqs, selectFreqs)
-        .sort(randomSort)
-        .filter((x,i) => i <= nRows * nCols - 1)
-        .map((l,i) => ({
-            letter: l,
-            x: i % 4,
-            y: Math.floor(i/4.0)
-        }))
+    
 
-    return board
+    const bag = bagOfLetters(freqs, selectFreqs)
+    const randomElement = (bag) => bag[Math.floor(random() * bag.length)]
+
+    let out = []
+    for (let i = 0; i < nCols; ++i) {
+      for (let j = 0; j < nRows; ++j) {
+        let row = {
+          letter:randomElement(bag),
+          y:i,
+          x:j,
+        }
+        out.push(row)
+      }
+    }
+    return out.flat()
 }
