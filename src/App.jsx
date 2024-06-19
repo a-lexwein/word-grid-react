1ab9d363ff10943c9ceab81edb27880b4d413dd1
Alex Wein <alexwein@gmail.com>
2024-06-19 07:30:55 -0700

    Layout tweaks (#3)
    * options button
    
    * small tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 import './App.css';
 
 import Grid from './components/Grid';
@@ -9,12 +11,18 @@ import names from '../data/freqs-name.json';
 import wordInList from '../helpers/wordInList';
 import score from '../helpers/score';
 
+
+
 function Main() {
   const { size, freqIndex, seed } = useParams();
   const [currentGuess, setGuess] = useState('');
-  const [timer, setTimer] = useState(60);
+  const [timer, setTimer] = useState(120);
   const [wordSubmitted, setWordSubmitted] = useState(false);
   const [history, updateHistory] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalRef = useRef(null);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -23,7 +31,6 @@ function Main() {
 
     return () => clearInterval(interval);
   }, []);
-
 
   const submitWord = () => {
     setWordSubmitted(true);  // Signal the word submission event
@@ -35,13 +42,12 @@ function Main() {
     updateHistory([...history, submission])
 
     setGuess('');  // Clear the current guess
-
   };
 
   const [options, setOptions] = useState({
-    nRows: size ? size.split('x')[0] : 4,
-    nCols: size ? size.split('x')[1] : 4,
-    gameLength: 60,
+    nRows: size ? size.split('x')[0] : 8,
+    nCols: size ? size.split('x')[1] : 6,
+    gameLength: 120,
     seed: seed ?? 'hello',
     freqs: freqIndex ? names[freqIndex] : 'TWL 8 - 10',
     mods: [],
@@ -51,16 +57,51 @@ function Main() {
     setOptions(newOptions);
     setTimer(newOptions.gameLength);
     updateHistory([]);  // Reset the history
+    setModalOpen(false);
   };
+
+  const handleNewGame = () => {
+    // add random string to seed
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let newSeed = '';
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      newSeed += characters[randomIndex];
+    }
+
+    const newOptions = {
+      ...options,
+      seed: newSeed,
+    }
+    // update url
+    navigate(`/${options.nRows}x${options.nCols}/${freqIndex}/${newSeed}`);
+    // update game
+    handleSetOptions(newOptions);
+  }
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      setModalOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (modalOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [modalOpen]);
 
   return (
     <div className="App">
-      <div className="sidebar">
-        <Options
-          options={options}
-          setOptions={handleSetOptions}
-        />
-      </div>
+      <div id="title">Basic Orthographic Grid Game</div>
+      <button id="newgame-button" onClick={handleNewGame}>	New Game</button>
+      <button id="options-button" onClick={() => setModalOpen(true)}>	&#x2699;&#xFE0F;</button>
       <div className="grid-container">
         <div>&#x1F551; {timer}</div>
         <History history={history}></History>
@@ -79,6 +120,18 @@ function Main() {
           {currentGuess}
         </div>
       </div>
+
+      {modalOpen && (
+        <div className="modal" style={{ display: 'flex' }}>
+          <div className="modal-content" ref={modalRef}>
+            <span className="close" onClick={() => setModalOpen(false)}>&times;</span>
+            <Options
+              options={options}
+              setOptions={handleSetOptions}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
